3364b91a6e371f4e4929660259b17e1b7173a408
mdm121 <milkiles.morrison@gmail.com>
2018-09-18 16:48:59 -0500

    Progress Made2

--- apps.js
+++ apps.js
@@ -1,12 +1,20 @@
 // $("#btnSubmit").click(function() {
 //     alert("This button has been clicked.");
 // });
-
-let textValue;
-
 $(document).ready(function (){
     validate();
     $('#text').change(validate);
+    
+});
+
+let textValue;
+$("body").append("<div></div>");
+
+$("input[type=submit]").click(function(e) {
+    e.preventDefault();
+    let textValue = $("#text").val();
+    // printText(textValue);
+
 });
 
 function validate(){
@@ -17,18 +25,16 @@ function validate(){
     };
 };
 
-$("input[type=submit]").click(function(e) {
-    e.preventDefault();
-    let textValue = $("#text").val();
-    printText(textValue);
+// function printText(e) {
+//     $("div").append("<h2>" + e + "</h2>");
+//     $("h2").hover(function() {
+//         $("h2").css({ "text-align": "center", "width": "200px", "height": "200px",
+//         "background-color": "red", "border-radius": "2em"});
+//     });
+// };
 
-});
-
-$("body").append("<div></div>");
-
-
-function printText(e) {
-    $("div").append("<h2>" + e + "</h2>");
-    
-};
+$('#myDiv').append("<ul id='newList'></ul>");
+for (cnt = 0; cnt < someList.length; cnt++) {
+  $("#newList").append("<li>"+someList[cnt].FirstName + ":" + someList[cnt].LastName+"</li>");
+}
 
